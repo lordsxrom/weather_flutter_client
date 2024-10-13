f6b9b9749fdbc92ba9456fb269653ca35ccc9849
shumskii.n <lordsxrom@gmail.com>
2024-10-13 22:51:49 +0300

    Make Aurora target

---
+++ aurora/flutter/generated_plugin_registrant.cpp
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+#include <flutter/flutter_aurora.h>
+#include <path_provider_aurora/path_provider_aurora_plugin.h>
+
+#include "generated_plugin_registrant.h"
+
+namespace aurora {
+void RegisterPlugins() {
+  flutter::PluginRegistrar* registrar = GetPluginRegistrar();
+  PathProviderAuroraPlugin::RegisterWithRegistrar(registrar);
+}
+}
